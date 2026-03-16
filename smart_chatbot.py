f67c469b7504e60bbd3dbf6bbb24f60d72760473
ashokjaat000010000-sketch <ashokjaat000010000@gmail.com>
2026-03-16 15:12:47 +0530

    Update model version from gemini-1.5-flash to gemini-1.0-pro

--- smart_chatbot.py
+++ smart_chatbot.py
@@ -10,7 +10,7 @@ user_input = st.text_input("Ask something")
 
 if user_input:
     try:
-        model = genai.GenerativeModel("gemini-1.5-flash")
+  model = genai.GenerativeModel("gemini-1.0-pro")
         response = model.generate_content(user_input)
         st.write(response.text)
     except Exception as e:
