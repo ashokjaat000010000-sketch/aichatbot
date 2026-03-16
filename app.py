d5e1cac2223e478857f5c04d3125b5d608480f9d
ashokjaat000010000-sketch <ashokjaat000010000@gmail.com>
2026-03-16 14:44:14 +0530

    Refactor comment for API key setup
    Updated comment for API key configuration.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 import os
 
-# Gemini API key
+# API key configure
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("gemini-1.5-flash")
